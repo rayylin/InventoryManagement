94d0cd1e07629bb45f96655605d31b0ebe597a07
rayl <dwadeflashback@gmail.com>
2025-02-24 00:56:41 -0500

    tmp

---
+++ InventoryManagement/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryManagement.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName ="nvarchar(12)")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        public string BankName { get; set; }
+        public string SWIFT { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ InventoryManagement/Models/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext>options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Transaction> Transactions { get; set;}
+    }
+}
