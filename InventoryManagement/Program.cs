d5de61b93cc93d9514bad07cb420e62b307fbaa9
rayl <dwadeflashback@gmail.com>
2025-02-24 22:13:00 -0500

    tmo

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// DI for DbContext
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,7 +17,6 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
