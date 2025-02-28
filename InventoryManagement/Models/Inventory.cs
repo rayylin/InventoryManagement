18676b42ac64a8583dd176b9544029e5f095af31
rayl <dwadeflashback@gmail.com>
2025-02-27 22:13:36 -0500

    update table

--- InventoryManagement/Models/Inventory.cs
+++ InventoryManagement/Models/Inventory.cs
@@ -1,11 +1,18 @@
-﻿namespace InventoryManagement.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryManagement.Models
 {
     public class Inventory
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+        public int ProductId { get; set; }
+        public string? StoreId { get; set; }
         public int Quantity { get; set; }
-        public string? SupplierId { get; set; }
+        public int SafetyStock { get; set; }
+
+        [Column(TypeName = "nvarchar(2)")]
+        public string? Status { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
+        public string? Comment { get; set; }
         public DateTime? UpdateTime { get; set; }
         public string? UpdateUser { get; set; }
 
--- InventoryManagement/Models/Transaction.cs
+++ InventoryManagement/Models/Transaction.cs
@@ -17,15 +17,17 @@ namespace InventoryManagement.Models
         public string ProductId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
-        [DisplayName("Product Name")]
+        [DisplayName("Store Id")]
         [Required(ErrorMessage = "Must fill! ")]
         [MaxLength(60, ErrorMessage = "Too long")]
-        public string ProductName { get; set; }
+        public string StoreId { get; set; }
 
         public int Quantity { get; set; }
+        [DisplayName("Unit Cost ($)")]
+        public double UnitCost { get; set; }
 
-        [Column(TypeName = "nvarchar(20)")]
-        public string Comment { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
+        public string Status { get; set; }
 
         public DateTime CreateTime { get; set; }
         public string CreateUser { get; set; }
